ef603d12c744a3115acf9111ad78ca843e6cc958
rlchethan <rlchethan0331@gmail.com>
2026-09-08 16:26:20 +0530

    JS completed project started

---
+++ student_task_management_portal/src/App.jsx
@@ -0,0 +1,10 @@
+import Welcome from "./components/welcome"
+
+function App(){
+    return(
+        <div>
+            <Welcome />
+        </div>
+    );
+}
+export default App;
---
+++ student_task_management_portal/src/components/Welcome.jsx
@@ -0,0 +1,7 @@
+function welcome() {
+    return (
+        <h1>Welcome to the Student Task Management Portal</h1>
+    );
+}
+
+export default welcome;
