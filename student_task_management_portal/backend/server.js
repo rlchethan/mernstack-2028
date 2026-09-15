1944eff30cc9caef7ad1ace12348cab26d065664
rlchethan <rlchethan0331@gmail.com>
2026-09-15 21:14:02 +0530

    Updated project files

--- student_task_management_portal/backend/server.js
+++ student_task_management_portal/backend/server.js
@@ -1,39 +1,52 @@
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 app.use(express.json());
 
 const tasks = [
-  {
-    id: 1,
-    title: "Learn React",
-    description: "Understanding Components",
-    status: "Completed",
-  },
-  {
-    id: 2,
-    title: "Learn JavaScript",
-    description: "Understanding Variables and Functions",
-    status: "Pending",
-  },
-  {
-    id: 3,
-    title: "Learn MongoDB",
-    description: "Understanding Databases",
-    status: "Pending",
-  },
+    {
+        id: 1,
+        title: "Learn React",
+        description: "Understanding Components",
+        status: "Completed"
+    },
+    {
+        id: 2,
+        title: "Learn JavaScript",
+        description: "Understanding Variables and Functions",
+        status: "Pending"
+    },
+    {
+        id: 3,
+        title: "Learn MongoDB",
+        description: "Understanding Databases",
+        status: "Pending"
+    }
 ];
 
+
+// Test backend
 app.get("/", (req, res) => {
-  res.send("backend is running");
+    res.send("Backend is running");
 });
 
+
+// Get all tasks
 app.get("/api/tasks", (req, res) => {
-  res.json(tasks);
+    res.json(tasks);
 });
 
-app.listen(5000, () => {
-  console.log("server is running on port 5000");
+
+app.listen(5001, () => {
+    console.log("Server is running on port 5001");
 });
+app.post("/api/tasks", (req, res) => {
+    const newTask = req.body;
+    tasks.push(newTask);
+    res.status(201).json(newTask);
+});
+
